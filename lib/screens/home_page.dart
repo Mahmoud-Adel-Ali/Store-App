d1585fdaeb1545db4fe17f549d3c0695c34ca789
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-05-14 13:01:20 -0700

    product card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,7 +14,7 @@ class HomePage extends StatelessWidget {
           centerTitle: true,
           elevation: 0,
           title: const Text(
-            "New Tren",
+            "New Trend",
             style: TextStyle(
               color: Colors.black,
               fontSize: 25,
@@ -19,10 +22,65 @@ class HomePage extends StatelessWidget {
           ),
           actions: [
             IconButton(
-              icon:const Icon(FontAwesomeIcons.cartPlus),
+              icon: const Icon(FontAwesomeIcons.cartPlus),
               onPressed: () {},
             ),
           ]),
+      body: Column(
+        children: [
+          Container(
+            height: 130,
+            width: 220,
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 0,
+                blurRadius: 40,
+                offset: const Offset(10, 10), // changes position of shadow
+              ),
+            ]),
+            child: Card(
+              color: Colors.white.withOpacity(1),
+              elevation: 12,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hand pag TY",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "\$ 225",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
